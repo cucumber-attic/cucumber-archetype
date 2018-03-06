96983201b29be5a9ba19ea224cdfe7f92199aabf
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-03-06 16:29:37 +0000

    Tweak compiler and add imports

--- src/main/resources/archetype-resources/src/test/java/Stepdefs.java
+++ src/main/resources/archetype-resources/src/test/java/Stepdefs.java
@@ -1,8 +1,10 @@
 package ${package};
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
 
 public class Stepdefs {
 }
