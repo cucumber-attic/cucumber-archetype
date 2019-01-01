1d664151a7fd79932e797f1680ac3202bf51fa16
M.P. Korstanje <rien.korstanje@gmail.com>
2019-01-01 17:28:56 +0100

    Add integration tests

---
+++ src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/Stepdefs.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+}
