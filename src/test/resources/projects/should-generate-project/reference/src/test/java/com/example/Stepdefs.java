1e7fa0baf74f92d2635556ee4f31029a8e94fd0e
mpkorstanje <logarithmicwhale@gmail.com>
2019-03-28 09:16:59 +0100

    Remove predefined steps and feature
    This archetype is used by the 10-Minute Tutorial and should
    initialize a truely empty project.

--- src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/Stepdefs.java
+++ src/test/resources/projects/should-generate-project/reference/src/test/java/com/example/Stepdefs.java
@@ -8,19 +8,4 @@ import static org.junit.Assert.*;
 
 public class Stepdefs {
 
-    @Given("Alice has installed maven")
-    public void alice_has_installed_maven() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-    @When("and she runs the maven command from the read me")
-    public void and_she_runs_the_maven_command_from_the_read_me() {
-        // Write code here that turns the phrase above into concrete actions
-    }
-
-
-    @Then("maven will setup a new cucumber project")
-    public void maven_will_setup_a_new_cucumber_project() {
-        // Write code here that turns the phrase above into concrete actions
-    }
 }
--- src/test/resources/projects/should-generate-project/reference/src/test/resources/com/example/cucumber.feature
+++
@@ -1,8 +0,0 @@
-Feature: Setting from up a Maven Archetype
-
-  To quickly start a new project a maven archetype is useful
-
-  Scenario: Initialize a project from the Maven Archetype
-    Given Alice has installed maven
-    When and she runs the maven command from the read me
-    Then maven will setup a new cucumber project
